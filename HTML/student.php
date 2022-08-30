b22cd53145adcf73bf58a55fe9d14cd7b4cede65
massoudrashid <112522529+massoudrashid@users.noreply.github.com>
2022-08-31 02:09:33 +0300

    Add files via upload

---
+++ HTML/student.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+<title>This is my titor</title>
+</head>
+<body>
+<h1>HTML FORM</h1>
+<form>
+	<input type="text" name="" placeholder="user_name"><br><br>
+	<input type="text" name="" placeholder="full_name"><br><br>
+	<input type="date" name="" placeholder="date_of_birth"><br><br>
+	<input type="text" name="" placeholder="your_address"><br><br>
+	<input type="number" name="" placeholder="phone_number"><br><br>
+	<label>Nationality</label><br>
+	<select>
+		<option>select</option>
+		<option>Zanzibar</option>
+		<option>Tanzania</option>
+		<option>Kenya</option>
+		<option>Uganda</option>
+	</select><br><br>
+	<label>Gender</label><br>
+	<select>
+		<option>select</option>
+		<option>male</option>
+		<option>fimele</option>
+	</select>
+
+</form>
+</body>
+</html>
